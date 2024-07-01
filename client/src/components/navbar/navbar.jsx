c25b1ab8c121fb6e2491dfd999e5fa3ad6cc902f
Evelyn Zhinin Tacuri <ezhinintacuri@ezhinin-ltm73cm.internal.salesforce.com>
2024-07-01 16:33:32 -0700

    added search image to navabar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './navbar.css';
 
+
 const Navbar = () => {
   return (
     <nav>
@@ -16,7 +17,7 @@ const Navbar = () => {
       
       <div className='search-box'>
         <input type="text" placeholder='Search' />
-        <img src="" alt="" />
+        <img src="src/assets/navbarSearch.png" alt="Search"  className='search-icon'/>
       </div>
       <a href="/createBoard" className="button-link">CreateBoard</a>
       
