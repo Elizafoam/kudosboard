add390cc638f2cd8fdd4e1de3e539e01dab1bb00
Maria Perez Chaparro <mariaperez@mariape-ltmt8yz.internal.salesforce.com>
2024-07-01 15:42:51 -0700

    cors and morgan implemented

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require("cors");
-const morgan = require("morgan");
+//const cors = require("cors");
+//const morgan = require("morgan");
 
 const app = express();
 const PORT = 3000;
@@ -11,8 +11,8 @@ const cardRoutes = require("../routes/cardRoutes");
 
 
 app.use(express.json());
-app.use(cors());
-app.use(morgan("dev"));
+//app.use(cors());
+//app.use(morgan("dev"));
 
 
 app.get('/', (req, res) => {
