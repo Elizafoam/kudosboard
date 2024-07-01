7dd16dc71eb43737f946487e8f6f37ab18a48ab1
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-01 14:03:14 -0700

    create banner

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+    return (
+        <div className="Banner">
+            <div className="text">kudosboard</div>
+        </div>
+    )
+}
+
+export default Banner;
