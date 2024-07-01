af75b8bc820b2849150ab6e271d07129c9d90c49
Evelyn Zhinin Tacuri <ezhinintacuri@ezhinin-ltm73cm.internal.salesforce.com>
2024-07-01 16:37:21 -0700

    created a navbar folder along footer and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+//import './App.css'
+import Navbar from './components/navbar/navbar';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  //   <div className='App'>
+  //     <header className='App-header'>
+  //     <div class='alert alert-primary' role='alert'>
+  //     <p style={{ display: "none" }} className='d-block'>
+  //       Bootstrap is now successfully installed 😃
+  //      </p>
+  //     <p className='d-none'>
+  //     Bootstrap is not installed if you can see this 😢
+  //     </p>
+  //     </div>
+  //     </header>
+  // </div>
+    <div className='container'>
+      <Navbar/>
+      
+    </div>
+  );
+  }
+  
+  
+  export default App;
