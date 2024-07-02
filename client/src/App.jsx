e4634310cec5caa6664a1e00a906a1d30d7eecf3
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-01 20:57:51 -0700

    include evelyn branch changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
+import NavBar from './components/navbar/navbar';
 import Banner from './components/Banner/Banner';
 import CardGrid from './components/CardGrid/CardGrid';
 import Footer from './components/Footer/Footer';
@@ -9,6 +10,7 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <Banner/>
       <CardGrid/>
       <Footer />
---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './navbar.css';
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='nav'>
+                <img src="src/assets/greeting-card.png" alt="logo"  className='weblogo'/>
+                <h1 className='navtext'>KudoBoard</h1>
+            </div>
+      
+            <div className='search-box'>
+                <input type="text" placeholder='Search ...' />
+                <img src="src/assets/glass.png" alt="Search"  className='search-icon'/>
+            </div>
+            <div>
+                <a href="/createBoard" className='button-link'>Create New Board</a>
+            </div>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
