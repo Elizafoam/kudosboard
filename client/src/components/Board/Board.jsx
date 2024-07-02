f6c7956e057effdfd35fb7a3a4a7c8c269d4e337
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-02 16:10:36 -0700

    implement create board

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -1,31 +1,13 @@
-// import React from "react";
-// import "./Board.css";
-
-// const Board = () => {
-//     return (
-//         <div className="board">
-//             <img src="/src/assets/spongebob.gif" alt="card image" />
-//             <h1>title</h1>
-//             <p>description</p>
-//             <div className="button">
-//                 <button>View Board</button>
-//                 <button>Delete Board</button>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Board;
-
 import React from "react";
 import "./Board.css";
 
-const Board = () => {
+const Board = ({title, category, author}) => {
     return(
         <div className="board">
             <img src="/src/assets/spongebob.gif" alt="card image" />
-            <h1>title</h1>
-            <p>description</p>
+            <h1>{title}</h1>
+            <p>{category}</p>
+            <p>By: {author} </p>
             <div className="button">
                 <button>View Board</button>
                 <button>Delete Board</button>
--- client/src/components/BoardPage/BoardPage.jsx
+++ client/src/components/BoardPage/BoardPage.jsx
@@ -1,15 +1,45 @@
+import { useState, useEffect } from "react";
 import './BoardPage.css';
+import axios from "axios";
 import NavBar from '../NavBar/NavBar'
 import Banner from '../Banner/Banner';
-import BoardGrid from '../BoardGrid/BoardGrid';
 import Footer from '../Footer/Footer';
+import Board from "../Board/Board";
 
 const BoardPage = () => {
+    const [boards, setBoards] = useState({})
+
+    const populateBoards = async (params = {}) => {
+        try {
+            const response = await axios.get("http://localhost:3000/boards", {params});
+            setBoards(response.data);
+
+        }
+        catch (error){
+            console.log("Error fetching boards", error);
+        }
+    }
+
+    useEffect(() => {
+        populateBoards();
+        
+      }, [boards]);
+    
+    
+
   return (
     <div className='BoardPage'>
       <NavBar />
       <Banner/>
-      <BoardGrid/>
+      <div className="b">
+        <div className="boardGrid">
+            {
+            Object.entries(boards).map((board) => (
+                <Board title={board[1]["title"]} category={board[1]["category"]} author={board[1]["author"]}/>
+            ))
+            }
+        </div>
+      </div>
       <Footer />
     </div>
   )
