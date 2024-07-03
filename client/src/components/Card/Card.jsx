6799deeeff22aa0d2616b6ce6bafefd2eed8c1a1
Evelyn Zhinin Tacuri <ezhinintacuri@ezhinin-ltm73cm.internal.salesforce.com>
2024-07-03 10:32:27 -0700

    upvote appears as display

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -27,7 +27,7 @@ import "./Card.css";
 
 const Card = ({ title, description, img_url, author, card_id }) => {
   const [deleted, setDeleted] = useState(false);
-  const [upvoted, setUpvoted] = useState(false)
+  const [upvotes, setUpvote] = useState(Card.upvotes || 0);
 
   const handleDelete = async () => {
     try {
@@ -43,9 +43,12 @@ const Card = ({ title, description, img_url, author, card_id }) => {
   //maybe
   const handleUpvote = async () => {
     try {
-      const response = await axios.put(`http://localhost:3000/boards/${card_id}/upvote`);
+      const response = await axios.put(`http://localhost:3000/boards/${board_id}/cards/${card_id}/upvote`, {
+        upvotes: upvotes +1,
+      }
+    );
       console.log("Card upvoted:", response.data);
-      setUpvoted(true); // Update local state to reflect upvote
+      setUpvote(upvotes +1); // Update local state to reflect upvote
     } catch (error) {
       console.error("Error upvoting card:", error);
       // Handle error state or notify user of upvote failure
@@ -67,9 +70,7 @@ const Card = ({ title, description, img_url, author, card_id }) => {
       <img src={img_url} alt="GIF" />
       <p>{author}</p>
       <div className="button">
-      <button onClick={handleUpvote} disabled={upvoted}>
-          {upvoted ? "Upvoted" : "Upvote"}
-        </button>
+      <button onClick={handleUpvote} > Upvote {upvotes}</button>
         <button onClick={handleDelete}>Delete</button>
       </div>
     </div>
