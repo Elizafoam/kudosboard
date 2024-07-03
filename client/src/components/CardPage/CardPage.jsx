3a59e2ced9e52551e4567139bed64be8ee5106ce
Evelyn Zhinin Tacuri <ezhinintacuri@ezhinin-ltm73cm.internal.salesforce.com>
2024-07-03 07:37:07 -0700

    cards can be populated with the giphy,made changes on card,crdPage,NewCard

--- client/src/components/CardPage/CardPage.jsx
+++ client/src/components/CardPage/CardPage.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './CardPage.css';
 import { useParams } from 'react-router-dom';
-import NavBar from '../NavBar/NavBar'
+import NavBar from '../NavBar/NavBar';
 import Banner from '../Banner/Banner';
 import Card from '../Card/Card';
 import Footer from '../Footer/Footer';
@@ -10,7 +10,8 @@ import NewCard from '../NewCard/NewCard';
 const CardPage = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [cards, setCards] = useState([]);
-  const { boardId } = useParams();
+  const { board_id } = useParams();
+
 
 
   const handleOpenModal = () => {
@@ -22,37 +23,94 @@ const CardPage = () => {
   };
 
   const handleCreateSuccess = (newCard) => {
-    console.log("New card created", newCard)
-    setCards([...cards, newCard]);
+    console.log("New card created", newCard);
+    setCards([...cards, newCard]); // Append newCard to the existing cards array
     handleCloseModal();
-  }
+  };
 
-  return (
-    <div className='CardPage'>
-      <h1>Card</h1>
-      <NavBar />
-      <Banner/>
-      <button className='button-link' onClick={handleOpenModal}>Create New Card</button>
-      {modalOpen && (
-        <NewCard
-          boardId={boardId}
-          onSuccess={handleCreateSuccess}
-          onClose={handleCloseModal}
+return (
+  <div className='CardPage'>
+    <h1>Cards</h1>
+    <NavBar />
+    <Banner />
+    <button className='button-link' onClick={handleOpenModal}>Create New Card</button>
+    {modalOpen && (
+      <NewCard
+        board_id={board_id}
+        onSubmit={handleCreateSuccess}
+        closeModal={handleCloseModal}
+      />
+    )}
+
+    <div className='CardGrid'>
+      {cards.map((card) => (
+        <Card
+          key={card.card_id}
+          card_id={card.card_id}
+          title={card.title}
+          description={card.description}
+          author={card.author}
+          img_url={card.img_url} // Add img_url if necessary
         />
-      )}
-
-      <div className="CardGrid">
-        {/* <Card /> */}
-        {cards.map((card) => (
-          <Card key={card.id} {...card} />
-        ))}
-      </div>
-      <Footer />
+      ))}
     </div>
-  )
-}
+    <Footer />
+  </div>
+);
+};
 
 export default CardPage;
 
+// import { useState } from 'react';
+// import './CardPage.css';
+// import { useParams } from 'react-router-dom';
+// import NavBar from '../NavBar/NavBar';
+// import Banner from '../Banner/Banner';
+// import Card from '../Card/Card';
+// import Footer from '../Footer/Footer';
+// import NewCard from '../NewCard/NewCard';
+// import axios from 'axios';
 
+// const CardPage = () => {
+//   const [modalOpen, setModalOpen] = useState(false);
+//   const [cards, setCards] = useState([]);
+//   const { boardId } = useParams();
+
+//   const handleOpenModal = () => {
+//     setModalOpen(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setModalOpen(false);
+//   };
+
+//   const handleAddCard = (newCard) => {
+//     setCards([...cards, newCard]);
+//     handleCloseModal();
+//   };
+
+//   return (
+//     <div className='CardPage'>
+//       <h1>Card</h1>
+//       <NavBar />
+//       <Banner />
+//       <button className='button-link' onClick={handleOpenModal}>Create New Card</button>
+//       {modalOpen && (
+//         <NewCard
+//           onSubmit={handleAddCard}
+//           closeModal={handleCloseModal}
+//           board_id={boardId}
+//         />
+//       )}
+//       <div className="CardGrid">
+//         {cards.map((card) => (
+//           <Card key={card.id} {...card} />
+//         ))}
+//       </div>
+//       <Footer />
+//     </div>
+//   )
+// }
+
+// export default CardPage;
 
